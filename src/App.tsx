63ce2e5477a4c46e05c5be10b7d3ba165d6a943b
k1arov <theslowryz@gmail.com>
2023-01-08 21:12:32 +0300

    Сделано отображение добавленных товаров

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Product} from "./components/Product";
 import {useProducts} from "./hooks/useProducts";
 import {Loader} from "./components/Loader";
 import {ErrorMessage} from "./components/ErrorMessage";
 import {Modal} from "./components/Modal";
 import {CreateProduct} from "./components/CreateProduct";
+import {IProduct} from "./models";
 
 function App() {
-    const {loading, error, products} = useProducts()
+    const {loading, error, products, addProduct} = useProducts()
+    const [modal, setModal] = useState(true)
+
+    const createHandler = (product: IProduct) => {
+        setModal(false)
+        addProduct(product)
+    }
     return (
         <div className="container mx-auto max-w-2xl pt-5">
             {loading && <Loader/>}
             {error && <ErrorMessage error={error}/>}
             {products.map(product => <Product product={product} key={product.id}/>)}
 
-            <Modal title="Create new product">
-                <CreateProduct/>
-            </Modal>
+            {modal && <Modal title="Create new product">
+                <CreateProduct onCreate={createHandler}/>
+            </Modal>}
         </div>
     )
 }
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -8,6 +8,10 @@ export function useProducts() {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState('');
 
+    function addProduct(product: IProduct) {
+        setProducts(prev => [...prev, product])
+    }
+
     async function fetchProducts() {
         try {
             setError('')
@@ -25,5 +29,5 @@ export function useProducts() {
     useEffect(() => {
         fetchProducts()
     }, [])
-    return { products, error, loading}
+    return { products, error, loading, addProduct}
 }
